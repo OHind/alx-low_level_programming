de896489a240be6f2d5f66d85e6a584ad22b4b55
OHind <ouchani.hind@gmail.com>
2023-04-21 16:22:26 -0700

    Adding spaces to teh printf

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,10 +11,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-		printf(n,"%d is positive");
+		printf(n , "%d is positive");
 	else if (n == 0)
-		printf(n,"%d is zero");
+		printf(n , "%d is zero");
 	else if (n < 0)
-		printf(n,"%d is negative");
+		printf(n , "%d is negative");
 	return (0);
 }
