2f481b9aade60223c5525ab408a18ccd2f55cef6
OHind <ouchani.hind@gmail.com>
2023-04-23 16:57:56 -0700

    trying a new datatype

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include <string.h>
 /**
  * main - entry point of the program
  * Return: returns 0
  */
 int main(void)
 {
-        char buffer[50] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        string buffer[50] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
         int i;
 
         for (i = 0; i < 51; i++)
