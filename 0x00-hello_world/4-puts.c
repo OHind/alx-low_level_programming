25af23034d0561b4f895ecd36eae47e2664b536b
OHind <ouchani.hind@gmail.com>
2023-04-23 03:58:00 -0700

    A c program that prints using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - the entry point of the program
+ * Return: Return 0 always successful
+ */
+int main(void)
+{
+	char str[51]
+
+	strcpy(str, "Programming is like building a multilingual puzzle\n");
+	puts(str);
+	return(0);
+}
