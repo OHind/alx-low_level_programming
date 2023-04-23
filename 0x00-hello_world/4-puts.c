a31e24bf59e1cd1a053670293abeae67d0b377a7
OHind <ouchani.hind@gmail.com>
2023-04-23 04:23:50 -0700

    correcting some betty style rules

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,7 +8,7 @@ int main(void)
 {
 	char str[52];
 
-	strcpy(str, "Programming is like building a multilingual puzzle\n");
+	strcpy(str, "Programming is like building a multilingual puzzle");
 	puts(str);
-	return(0);
+	return (0);
 }
