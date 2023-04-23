1b10532ce0c6d8943dd50abd43f8a1959538995d
OHind <ouchani.hind@gmail.com>
2023-04-23 05:01:58 -0700

    Adding the quotes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,7 +8,7 @@ int main(void)
 {
 	char str[54];
 
-	strcpy(str, "Programming is like building a multilingual puzzle");
+	strcpy(str, "\"Programming is like building a multilingual puzzle");
 	puts(str);
 	return (0);
 }
