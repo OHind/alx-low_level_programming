9117e4d2c308ba0f1de39c495291cb22c2927299
OHind <ouchani.hind@gmail.com>
2023-04-23 16:42:46 -0700

    Correcting some errors

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 /**
- *
  * main - entry point of the program
- *
  * Return: returns 0
  */
 int main(void)
@@ -10,9 +8,10 @@ int main(void)
 	char buffer[26] = "abcdefghijklmnopqrstuvwxyz";
 	int i;
 
-	for(i = 0; i < 26; i++)
+	for( i = 0; i < 26; i++)
 	{
 		putchar(buffer[i]);
 	}
+	putchar('\n');
 	return (0);
 }
