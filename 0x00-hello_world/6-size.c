c2650085988c9dd77045b7acddec22170493a4ac
OHind <ouchani.hind@gmail.com>
2023-04-23 09:50:43 -0700

    Correcting the printf function

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,10 @@ int main(void)
 	long long lli;
 	float f;
 
-	printf("Size of a char: %d", sizeof(c), "byte\(s\)\n");
-	printf("Size of an int: %d", sizeof(i), "byte\(s\)\n");
-	printf("Size of a long int: %d", sizeof(li), "byte\(s\)\n");
-	printf("Size of a long long int: %d", sizeof(lli), "byte\(s\)\n");
-	printf("Size of a float: %d", sizeof(f), "byte\(s\)\n");
+	printf("Size of a char: %d bytes(s)\n", sizeof(c));
+	printf("Size of an int: %d byte(s)\n", sizeof(i));
+	printf("Size of a long int: %d byte(s)\n", sizeof(li));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(lli));
+	printf("Size of a float: %d byte(s)\n", sizeof(f));
 	return (0);
 }
