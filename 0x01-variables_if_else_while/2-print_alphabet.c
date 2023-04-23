a6b0584d5e8f6f0fa73f9af622b70c415221cdb2
OHind <ouchani.hind@gmail.com>
2023-04-23 16:36:08 -0700

    Print the alphabet in lower cases

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio>
+/**
+ *
+ * main - entry point of the program
+ *
+ * Return: returns 0
+ */
+int main(void)
+{
+	char buffer[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for(i = 0; i < 26; i++)
+	{
+		putchar(buffer[i]);
+	}
+	return (0);
+}
