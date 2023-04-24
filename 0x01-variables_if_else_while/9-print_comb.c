a245086d490b98b11ecd58b8624f581099c44ff3
OHind <ouchani.hind@gmail.com>
2023-04-24 11:29:00 -0700

    print combo

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main -entry point of the program
+ * Return: returns 0
+ */
+int main(void)
+{
+	int i;
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i);
+		putchar(", ");
+	}
+	putchar(i);
+	putchar('\n');
+	return (0);
+}
