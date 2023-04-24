25a6897dce5e81d4288f71fa9f0f4cb4679f6729
OHind <ouchani.hind@gmail.com>
2023-04-23 17:08:30 -0700

    Minor changes

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 /**
  * main - entry point of the program
- * Return: returns 0
+ * Return: returns 0 always
  */
 int main(void)
 {
-        char buffer1[26] = "abcdefghijklmnopqrstuvwxyz";
+	char buffer1[26] = "abcdefghijklmnopqrstuvwxyz";
 	char buffer2[26] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	int i;
 	int j;
 
-        for (i = 0; i < 26; i++)
-        {
-                putchar(buffer1[i]);
-        }
+	for (i = 0; i < 26; i++)
+	{
+		putchar(buffer1[i]);
+	}
 	for (j = 0; j < 26; j++)
 	{
 		putchar(buffer2[j]);
 	}
-        putchar('\n');
-        return (0);
+	putchar('\n');
+	return (0);
 }
