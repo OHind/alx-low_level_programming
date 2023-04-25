e482c783433219dcdcb820183e0daf05075714e4
OHind <ouchani.hind@gmail.com>
2023-04-25 15:38:04 -0700

    Check if a caracter is a lower case

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * islower - check if a caracter is a lower case
+ * Return: 1 or 0
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
