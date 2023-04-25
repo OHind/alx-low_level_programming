a7ba63ed19a6c318835178e7a0c870336292ff90
OHind <ouchani.hind@gmail.com>
2023-04-25 15:47:48 -0700

    check if a caracter is an alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isalpha - check if a caracter is a lower case
+ * @c: the caracter in ASCII code
+ * Return: 1 or 0
+ */
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
