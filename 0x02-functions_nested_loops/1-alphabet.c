143d3bb77ac832e427ba51b8f5af33289f046d44
OHind <ouchani.hind@gmail.com>
2023-04-25 13:40:31 -0700

    Printing the alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *
+ * print_alphabet - prints the alphabet 
+ *
+ * Return: void
+ *
+ */
+void print_alphabet(void)
+{
+	int i = 0;
+
+	for (i = 97; i <= 122; i++)
+	{
+		_puchar(i);
+	}
+	_putchar('\n');	
+}
