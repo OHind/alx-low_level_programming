647657ca3d051204b3b0630ce2d203b285b7a11f
OHind <ouchani.hind@gmail.com>
2023-04-27 16:02:56 -0700

    multiplication of two arguments

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiply
+ * @a: first argument
+ * @b: second argument
+ * Return: multiplication of two arguments
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
