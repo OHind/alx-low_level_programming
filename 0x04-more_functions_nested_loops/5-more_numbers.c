b78b128386b63a3ee2a539ada80d26fb5fe46111
OHind <ouchani.hind@gmail.com>
2023-04-28 13:59:51 -0700

    Print more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,25 +1,37 @@
 #include "main.h"
 
 /**
- * more_numbers - prints numbers 0-9
+ * more_numbers - prints numbers 0-14 ten times
  *
  * Return: void
  */
 
 void more_numbers(void)
 {
-	int c;
-	int k = 0;
+	int line, n, repeat, limit;
 
-	while (k < 10)
+	n = '0';
+	repeat = 0;
+	limit = '9';
+
+	for (line = 0; line < 10; line++)
 	{
-		for (c = 0; c <= 14; c++)
+		while (repeat < 2)
 		{
-			if (c >= 0)
-				_putchar(c / 10 + '0');
-			_putchar(c % 10 + '0');
+			while (n <= limit)
+			{
+				if (limit == '4')
+					_putchar('1');
+				_putchar(n);
+				n++;
+			}
+			repeat++;
+			limit = '4';
+			n = '0';
 		}
 		_putchar('\n');
-		k++;
+		repeat = 0;
+		n = '0';
+		limit = '9';
 	}
 }
