1eb869f27a95c180d0248064fde61529b7e36814
OHind <ouchani.hind@gmail.com>
2023-05-02 16:23:58 -0700

    copy a string to an other

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -5,4 +5,20 @@
  * @src: the source string
  * Return: the destination string
  */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+	int l = 0;
 
+	while (*(src + l) != '\0')
+	{
+		l++;
+	}
+	while (i < l)
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	return (dest);
+
+}
