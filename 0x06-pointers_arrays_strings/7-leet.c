7b56a10068200837a7e670822e5a5aaaa18c5b7f
OHind <ouchani.hind@gmail.com>
2023-05-05 16:10:49 -0700

    only one if

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -6,21 +6,20 @@
  */
 char *leet(char *c)
 {
-	int i = 0;
+	int i, j;
 
-	while (*(c + i) != '\0')
+	char *src = "aAeEoOtTlL";
+	char *des = "4433007711";
+
+	for (i = 0; *(c + i) != '\0'; i++)
 	{
-	if (*(c + i) == 'a' || *(c + i) == 'A')
-		*(c + i) = '4';
-	else if (*(c + i) == 'e' || *(c + i) == 'E')
-                *(c + i) = '3';
-	else if (*(c + i) == 'o' || *(c + i) == 'O')
-                *(c + i) = '0';
-	else if (*(c + i) == 't' || *(c + i) == 'T')
-                *(c + i) = '7';
-	else if (*(c + i) == 'l' || *(c + i) == 'L')
-                *(c + i) = '1';
-	i++;
+		for (j = 0; j < 10; j++)
+		{
+			if (*(c + i) == *(src + j))
+			{
+				*(c + i) = *(des + j);
+			}
+		}
 	}
 	return (c);
 }
