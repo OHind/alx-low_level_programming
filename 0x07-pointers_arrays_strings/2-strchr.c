4a9be85bd4b6bb97b93eaa1eb3d89f8e78ab6c63
OHind <ouchani.hind@gmail.com>
2023-05-08 15:14:42 -0700

    First occurnce

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#define NULL 0
+/**
+ * _strchr - the occurence of a caracter
+ * @s: the caracter to look for the caracter into
+ * @c: the caracter to look for
+ * Return: the first occurence of the caracter
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		if (*(s + i) == c)
+			return (s + i);
+		i++;
+	}
+	return (NULL);
+}
