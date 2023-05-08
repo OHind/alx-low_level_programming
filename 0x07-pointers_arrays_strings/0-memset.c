aa93aefd49b4382037f9babf57f9b6e9bddc5c28
OHind <ouchani.hind@gmail.com>
2023-05-08 14:08:14 -0700

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _memset - Fill a block of memory with a specific value
+ * @s: the string to modify
+ * @b: caracter to add
+ * @n: Number of bits to be filled
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		*(s + i) = b;
+	}
+	return (s);
+}
