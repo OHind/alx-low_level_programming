3f706caf2894bfefb0e74f543258428b874de4e3
OHind <ouchani.hind@gmail.com>
2023-05-08 14:53:31 -0700

    memcopy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _memcpy - copy a string into another
+ * @dest: the string to copy to
+ * @src: the string to copy to
+ * @n: number of bytes to copy
+ * Return: the modified string
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0, i < n; i++)
+	{
+		*(dest + i) = *(src + i);
+	}
+	return (dest);
+}
