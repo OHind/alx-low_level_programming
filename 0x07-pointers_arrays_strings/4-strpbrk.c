6e2ee429d4294cacbca53651fac74c80195b10e2
OHind <ouchani.hind@gmail.com>
2023-05-09 15:47:36 -0700

    return a string starting from the first occurence

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include "main.h"
+DEFINE NULL 0
+/**
+ * _strpbrk - detect the first occurrence of any caracter of the argument
+ * @s: the source string
+ * @accept: the check
+ * Return: the source string starting from the occurence
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i = 0;
+	int j = 0;
+
+	while (*(s + i) != '\0')
+	{
+		while (*(accept + j) != '\0')
+		{
+			if (*(s + i) == *(accept + j))
+			{
+				return (s + i);
+			}	
+			j++;
+		}
+		i++;
+	}
+	return (NULL);
+
+}
