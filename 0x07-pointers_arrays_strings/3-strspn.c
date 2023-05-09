63b9ec0d993754a52a04c95d3d7ff458d5333401
OHind <ouchani.hind@gmail.com>
2023-05-09 15:35:42 -0700

    Minor modification

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -15,7 +15,7 @@ unsigned int _strspn(char *s, char *accept)
 	while (*(s + i) != '\0')
 	{
 		j = 0;
-		fnum = num
+		fnum = num;
 		for (j = 0; *(accept + j); j++)
 		{
 			if (*(s + i) == *(accept + j))
