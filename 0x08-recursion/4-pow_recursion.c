503d9b298ac0081f3e78f85a728e2abd1cc930c1
OHind <ouchani.hind@gmail.com>
2023-05-10 16:48:48 -0700

    POWER

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _pow_recursion - te power
+ * @x: first argument
+ * @y: second argument
+ * Return: a value
+ */
+int _pow_recursion(int x, int y)
+{
+	if ( y >= 0)
+	{
+		return (x * int _pow_recursion(x , y-1);
+	}
+	else if (y == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		return (-1);
+	}
+}
