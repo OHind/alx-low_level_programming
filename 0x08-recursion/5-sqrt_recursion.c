59b70e930fee13efb2d26d775d0ae090301951e7
OHind <ouchani.hind@gmail.com>
2023-05-11 15:35:16 -0700

    sqrt

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,2 +1,34 @@
 #include "main.h"
 /**
+ * _sqrt_recursion - the square root of a number
+ * @n: the number
+ * Return: The square root
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (_check_sqrt(n, 0));
+	}
+
+}
+
+
+/**
+ * _check_sqrt - the square root of a number
+ * @p: number to calculate the square number
+ * @j: natural to iterate
+ * Return: The square root
+ */
+int _check_sqrt(int p, int j)
+{
+	if (j * j > p)
+		return (-1);
+	if (j * j == p)
+		return (j);
+	return (_check_sqrt(p, j + 1));
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -4,3 +4,7 @@ void _print_rev_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
+int _check_sqrt(int p, int j)
