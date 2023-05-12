050e426eabd1d58788b30c13ea08e21a01a8c93e
OHind <ouchani.hind@gmail.com>
2023-05-12 15:22:27 -0700

    Theories

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,9 +8,11 @@
  */
 int main(int argc, char *argv[])
 {
+int main(int argc, char *argv[])
+{
 	if (argc <= 2 || argc > 3)
 	{
-		printf("ERROR\n");
+		printf("Error\n");
 		return (1);
 	}
 	else
@@ -18,4 +20,5 @@ int main(int argc, char *argv[])
 		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 		return (0);
 	}
+}
 }
