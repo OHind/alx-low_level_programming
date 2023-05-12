a5b3fba98ae54fb9205d09dcfe54786fa55787a9
OHind <ouchani.hind@gmail.com>
2023-05-12 11:55:07 -0700

    agrc argument not used

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,7 +6,7 @@
  * @argv - the array to store arguments
  * Return: 0
  */
-int main (int argc, char *argv[])
+int main (int argc __attribute__((unused)), char *argv[])
 {
 	printf ("The name of the argument is %s \n", argv[0]);
 	return 0;
