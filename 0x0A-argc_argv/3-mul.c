2431ffbae2bfd85b5c30840666a5d3a4e0c3e9cf
OHind <ouchani.hind@gmail.com>
2023-05-12 15:18:46 -0700

    Some modification

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -6,7 +6,7 @@
  * @argv: the array of arguments
  * Return: Return the result of the multiplication of two integers
  */
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc, char *argv[])
 {
 	if (argc <= 2 || argc > 3)
 	{
