72d9804cdf24885b2c0b60e140ec30dbcbd45d9f
OHind <ouchani.hind@gmail.com>
2023-05-12 12:21:10 -0700

    Add the read me file

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
 /**
  * main - to print the program's name
  * @argc: numebr of arguments
@@ -8,7 +8,6 @@
  */
 int main(int argc __attribute__((unused)), char *argv[])
 {
-	printf("%s\n", *argv);
-
+	printf("The name of the argument is %s \n", *argv);
 	return (0);
 }
