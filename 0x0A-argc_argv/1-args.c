aebec4e01b77233cbb7e40cc1eee4f4f78254811
OHind <ouchani.hind@gmail.com>
2023-05-12 14:57:31 -0700

    remove some spaces

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,8 +5,8 @@
  * @argv: array of arguments
  * Return: 0
  */
-int main (int argc, char **argv __attribute__((unused)))
+int main(int argc, char **argv __attribute__((unused)))
 {
-	printf("%d \n", argc);
+	printf("%d \n", argc - 1);
 	return (0);
 }
