d5074d892cb8630398d7fbc4ccd33d139bb41d08
OHind <ouchani.hind@gmail.com>
2023-05-12 16:31:26 -0700

    Adding

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,21 +1,62 @@
 #include <stdio.h>
+#include <stdlib.h>
+/**
+ * check_int - is input a number
+ * @s: argument to verufy
+ * Return: 1 or 0
+ */
+
+int check_int(char *s)
+{
+	int i;
+
+	i = 0;
+	while (*(s + i) != '\0')
+	{
+		if (*(s + i) >= '0' && *(s + i) <= '9')
+		{
+			i++;
+		}
+		else
+		{
+			return (0);
+		}
+	}
+	return (1);
+}
 /**
  * main - adding a positive number
  * @argc: Number of arguments
  * @argv: array of arguments
- * Return: 0
+ * Return: 0 or 1
  */
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	int i = 0;
-	int add = 0;
+	int i, sum, is_num;
 
+	sum = 0;
 	if (argc == 1)
-		printf ("ERROR \n");
-	else
-		for (i = 0; i < argc; i++)
+	{
+		printf("0\n");
+	}
+	else if (argc > 1)
+	{
+		i = 1;
+		while (i < argc)
 		{
-			add += argv[i];
+			is_num = check_number(argv[i]);
+			if (is_num == 1)
+			{
+				sum += atoi(argv[i]);
+			}
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+			i++;
 		}
-		printf ("the addition of arguments is %d \n ",add);
+		printf("%d\n", sum);
+	}
+	return (0);
 }
