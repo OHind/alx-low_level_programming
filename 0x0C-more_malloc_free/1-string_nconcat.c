3d5e7b9ac5d4d8075076f21e770e5bba03e9688e
OHind <ouchani.hind@gmail.com>
2023-05-19 15:15:24 -0700

    minor modification

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -7,7 +7,6 @@
  * @n: number
  * Return: pointer
  */
-
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *c;
@@ -15,27 +14,19 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (s1 == NULL)
 		s1 = "";
-
 	if (s2 == NULL)
 		s2 = "";
-
 	for (i = 0; s1[i]; i++)
 		l++;
-
 	c = malloc(sizeof(char) * (l + 1));
-
 	if (c == NULL)
 		return (NULL);
-
 	l = 0;
-
 	for (i = 0; s1[i]; i++)
 		c[l++] = s1[i];
 
 	for (i = 0; s2[i] && i < n; i++)
 		c[l++] = s2[i];
-
-	concat[l] = '\0';
-
+	c[l] = '\0';
 	return (c);
 }
