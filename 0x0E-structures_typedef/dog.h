39b74e01859ddb9859fed17445da1e1ca41d4ef1
OHind <ouchani.hind@gmail.com>
2023-05-22 13:24:18 -0700

    structure

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ * struct dog - dog
+ * @name: Name of the dog
+ * @age: age of teh dog
+ * @owner: Owner of the dog
+ *
+ * Description: describes a dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
