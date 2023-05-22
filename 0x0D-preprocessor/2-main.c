6d6da3e424904523ab9cb90b624ffc4c3c870668
OHind <ouchani.hind@gmail.com>
2023-05-22 12:43:24 -0700

    Some betty adaptation

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-
 /**
  * main - main function
  * Return: Always success
  */
 int main(void)
 {
-	printf("%s\n",__FILE__);
-       return (0);	
+
+	printf("%s\n", __FILE__);
+	return (0);
 }
