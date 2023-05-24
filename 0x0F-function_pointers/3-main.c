fca90e34a2cc145199513bfcdad7d73a32003d09
OHind <ouchani.hind@gmail.com>
2023-05-24 14:38:31 -0700

    pointer to function operation

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,31 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - the entry point of a program
+ * @argc: number of aruments
+ * @argc: array of arguments
+ * Return: always success
+ */
+int main(int argc, char *argv[])
+{
+	int x, y, r;
+	int (*op)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	op = get_op_func(argv[2]);
+	if (op == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	x = atoi(argv[1]);
+	y = atoi(argv[2]);
+	r = op(x, y);
+	printf("%d\n", r);
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,52 @@
+#include "3-calc.h"
+#include <stdlib.h>
+/**
+ * op_add - returns the addition of two integers
+ * @a: first operand
+ * @b: second operand
+ * Returns: the addition of a and b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - returns the soustraction of two integers
+ * @a: first operand
+ * @b: second operand
+ * Returns: the soustraction of a and b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - returns the mul of two integers
+ * @a: first operand
+ * @b: second operand
+ * Returns: the mul of a and b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - returns the div of two integers
+ * @a: first operand
+ * @b: second operand
+ * Returns: the div of a and b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - returns the addition of two integers
+ * @a: first operand
+ * @b: second operand
+ * Returns: the modulo of a and b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
