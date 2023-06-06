ce8c82f6d063e3a66b722a7e656b31204a98f53a
OHind <ouchani.hind@gmail.com>
2023-06-06 13:33:34 -0700

    print numeber of elements of a linked list

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * listint_len - lenght of a singly linked list
+ * @h: the linked list
+ * Return: the sise of the linked list
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t i = 0;
+
+	if (h == NULL)
+	{
+		return (NULL);
+	}
+	while (h->next != NULL)
+	{
+		i++;
+	}
+	return (i);
+}
