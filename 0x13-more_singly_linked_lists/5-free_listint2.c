87a2a3fb7cd5896bec7b77e2ac7425efca6f0e68
OHind <ouchani.hind@gmail.com>
2023-06-06 14:49:41 -0700

    free a list with settinh the list to NULL

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,24 +1,25 @@
 #include "lists.h"
-
+#include <stdlib.h>
 /**
  * free_listint2 - frees a list_t list
  * @head: head of a linked list
  * Return: void
  */
 
-void free_list(list_t **head)
+void free_listint2(listint_t **head)
 {
-	list_t *temp;
+	listint_t *temp;
 
 	if (head == NULL)
 	{
 		return;
 	}
-	while (head != NULL)
+	temp = *head;
+	while (temp != NULL)
 	{
-		temp = head->next;
-		free
-		free(head);
-		head = temp;
+		temp = temp->next;
+		free(*head);
+		*head = temp;
 	}
+	*head = NULL;
 }
